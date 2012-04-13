7aa60c9678744f2d16a98dfe8606c98d6999efd2
Dag Odenhall <dag.odenhall@gmail.com>
2012-04-13 20:50:57 +0000

    happstack-jmacro: prevent cross-request global namespace pollution

--- Happstack/Server/JMacro.hs
+++ Happstack/Server/JMacro.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 -- |This modules provides support for using JMacro with Happstack.
 --
 -- It provides the instance,
@@ -5,14 +7,23 @@
 -- > instance ToMessage JStat 
 --
 -- Which will serve a 'JStat' value as @text/javascript; charset=UTF-8@.
+-- The rendered JavaScript will be wrapped in an anonymous function that is
+-- then called, so as to ensure the statements execute in a local scope.
+-- An implication of this is that top-level unhygienic variables in JMacro
+-- will /not/ be globally available; instead, you should set properties on
+-- the global @window@ object.
 module Happstack.Server.JMacro where
 
 import qualified Data.ByteString.Char8 as S
 import Data.ByteString.Lazy.UTF8       as LB
 import Happstack.Server                (ToMessage(..))
-import Language.Javascript.JMacro      (JStat(..), renderJs)
+import Language.Javascript.JMacro      (JStat(..), renderJs, jmacro, jLam, toStat)
 import Text.PrettyPrint                (Style(mode), Mode(OneLineMode), style, renderStyle)
 
 instance ToMessage JStat where
     toContentType _  = S.pack "text/javascript; charset=UTF-8"
-    toMessage     js = LB.fromString . renderStyle (style { mode = OneLineMode }) . renderJs $ js
+    toMessage     js =
+        LB.fromString . renderStyle lineStyle . renderJs $ scoped
+      where
+        lineStyle = style { mode = OneLineMode }
+        scoped    = [jmacro| (function { `(js)`; })(); |]
